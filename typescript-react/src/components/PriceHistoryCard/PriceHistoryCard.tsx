037c871ffe1c98a136c4778f2fec330bb7d55726
carvalhoju <julio.deoliveira-carvalho@jsmd-group.com>
2023-05-07 16:11:54 +0200

    moved prices history query to card component

--- typescript-react/src/components/PriceHistoryCard/PriceHistoryCard.tsx
+++ typescript-react/src/components/PriceHistoryCard/PriceHistoryCard.tsx
@@ -1,6 +1,25 @@
 import styles from './price-history-card.module.scss';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
+const fetchListingPrices = (
+  listingId?: string,
+): Promise<{ created_date: string; price_eur: number }[]> => {
+  if (!listingId) return Promise.resolve([]);
+
+  return axios
+    .get(`http://localhost:8080/listings/${listingId}/prices`)
+    .then((res) => res.data);
+};
 const PriceHistoryCard = () => {
+  const { listingId } = useParams();
+  const { data, status } = useQuery({
+    queryKey: ['listingsPrices'],
+    queryFn: () => fetchListingPrices(listingId),
+  });
+
+  console.log(data, status);
   return (
     <div className={styles['container']}>
       <table className={styles['price-card']}>
